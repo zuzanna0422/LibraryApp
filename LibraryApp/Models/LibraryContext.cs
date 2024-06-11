eeecb7c1048cff0d03de5e8f7058f3b155b8fc58
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2024-06-11 15:35:50 +0200

    Configure entity relationships in OnModelCreating

--- LibraryApp/Models/LibraryContext.cs
+++ LibraryApp/Models/LibraryContext.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
-namespace LibraryApp.Models
+public class LibraryContext : DbContext
 {
-    class LibraryContext
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Categories> Categories { get; set; }
+    public DbSet<Borrowing> Borrowings { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Library;Trusted_Connection=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ModelBuilder.Entity<Categories>()
+            .HasMany(c => c.Books)
+            .WithOne(b => b.Category)
+            .HasForeignKey(b => b.Category_Id);
+
+        ModelBuilder.Entity<Book>()
+            .HasMany(b => b.Borrowings)
+            .WithOne(br => br.Book)
+            .HasForeignKey(br => br.Book_Id);
+
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Borrowings)
+            .WithOne(br => br.User)
+            .HasForeignKey(br => br.UserId);
     }
 }
+
